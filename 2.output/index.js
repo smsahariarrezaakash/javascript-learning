dc06eb4aa01c03bda2499459ae5b398deaaeeed9
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-05 00:39:18 +0600

    output added

---
+++ 2.output/index.js
@@ -0,0 +1,27 @@
+/*
+To see output in the console we need to 5 things:
+a) write console
+b) use a dot sign (.)
+c) write log()
+d) use parenthesis ()
+e) write what you want to show as output in the parenthesis
+*/
+
+const age = 28;
+console.log(age);
+
+const booksToRead = 100;
+console.log(booksToRead);
+
+let wakeUpTime = 6.3;
+wakeUpTime = 8.4;
+console.log(wakeUpTime);
+
+const wastedTime = 2;
+console.log(wastedTime);
+
+const borrow = 2500;
+console.log(borrow);
+
+const pillow = 9;
+console.log(pillow);
