57e368ee2d0bc0e45b5bebcc9898aafc82043870
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-06 02:15:50 +0600

    Math Gangster hoye gechi

---
+++ 5.MathGangstar/index.js
@@ -0,0 +1,52 @@
+const income = 100;
+const expense = 40;
+const remaining = income - expense;
+console.log(remaining);
+
+const totalPencil = 10;
+const pencilUsePerTime = 2;
+const TotalUseOfPencil = totalPencil / pencilUsePerTime;
+console.log(TotalUseOfPencil);
+
+const totalMoneyIHave = 60;
+const expensePerTime = 15;
+const times = totalMoneyIHave / expensePerTime;
+const remainingMoney = totalMoneyIHave % expensePerTime;
+console.log(times, remainingMoney);
+
+const word1 = "Hello";
+const word2 = "World";
+const sentence = word1 + word2;
+console.log(sentence);
+
+const chal = 153;
+const dal = 261;
+const total = chal + dal;
+console.log(total);
+
+const money = 500;
+const expensePerTime2 = 75;
+const time2 = money / expensePerTime2;
+const remainingMoney2 = money % expensePerTime2;
+console.log(time2, remainingMoney2);
+
+const number1 = 8;
+const number2 = 3;
+const result1 = number1 / number2;
+const result2 = number1 % number2;
+console.log(result1, result2);
+
+const number3 = 50;
+const number4 = 9;
+const result3 = number3 % number4;
+console.log(result3);
+
+const text3 = "Bangla";
+const text4 = "desh";
+console.log(text3 + text4);
+
+const riceStock = 480;
+const riceIEatDaily = 4;
+const calculationPerDay = riceStock / riceIEatDaily;
+const calculationPerMonth = calculationPerDay / 30;
+console.log(calculationPerMonth);
