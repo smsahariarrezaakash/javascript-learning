6b7c6d76d1251f8dda697fdc202a1cbf54fde9d5
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-05 22:18:46 +0600

    String Learned

---
+++ 3.String/index.js
@@ -0,0 +1,20 @@
+const name = "Sahariar";
+console.log(name);
+
+const address = "Agrabad, Chattogram";
+console.log(address);
+
+const bestFriendCollege = "Govt. Commerce College, Chattogram";
+console.log(bestFriendCollege);
+
+const fan = "Elizabeth Olsen";
+console.log(fan);
+
+const movieOfTheYear = "One Piece: Red";
+console.log(movieOfTheYear);
+
+const hobby = "Watching Anime & Donghua";
+console.log(hobby);
+
+const color = "black";
+console.log(color);
