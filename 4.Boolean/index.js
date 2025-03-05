fd7f30f4fed57bc6be506ac62d63c4dd65c3c8ca
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-05 22:30:11 +0600

    boolean learned

---
+++ 4.Boolean/index.js
@@ -0,0 +1,22 @@
+const isFriendHappy = false;
+console.log(isFriendHappy);
+
+const crushReplied = false;
+console.log(crushReplied);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
