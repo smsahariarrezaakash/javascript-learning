4a6b327b07ae0e8c48bb7a3a8ed163a878679517
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-06 23:45:29 +0600

    Let Learned

---
+++ 7.Let/index.js
@@ -0,0 +1,28 @@
+let age = 71;
+console.log(age);
+age = 28;
+console.log(age);
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+let studyTime = 4;
+studyTime = 2;
+console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = 3 - 2;
+console.log(friendsCount);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+let chairsCount = 40;
+chairsCount = 40 - 2 - 6;
+console.log(chairsCount);
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
