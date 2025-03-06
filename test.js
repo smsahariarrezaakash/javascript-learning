0e8b4ff1e9441fa4cc6d8106c61fdf18040ea5a2
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-07 01:38:00 +0600

    comment Learned

---
+++ test.js
@@ -0,0 +1,34 @@
+const score = 70; // Math Test Score
+
+// Earth's equatorial radius is 6378 km
+// Its polar radius is 6357 km
+// Orbital period: 365 days
+const radius = 6378;
+
+/*
+My favorite dress are:
+shirt
+pant
+panjabi
+suit/blazer
+*/
+const dress = 4;
+
+// I got gift 100 tk as Eid Salami
+const giftMoney = 100;
+
+// I am going to study for 7 hours.
+// First 2 hours for Math
+// Next 2 hours for English
+// Next 2 hours for Islamic Religion
+// Next 1 hour for Web Development
+const studyTime = 7;
+
+/*
+Sending Money to Mainul
+He will get 5000 tk Service Charge 100 tk
+*/
+const sendMoney = 5000;
+
+// Pay 2570 tk. 5 Islamic Books = 2500 tk & delivery charge 70 tk
+const pay = 2570;
