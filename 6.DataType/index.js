e6f63cf807de56dab8b55a91832c8be2c5d3ec16
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-06 23:27:16 +0600

    Data Type Learned

---
+++ 6.DataType/index.js
@@ -0,0 +1,17 @@
+const price = 555;
+console.log(typeof price);
+
+const learning = true;
+console.log(typeof learning);
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+const temperature = 102.5;
+console.log(typeof temperature);
+
+const isHungry = true;
+console.log(typeof isHungry);
+
+const isRich = true;
+console.log(typeof isRich);
