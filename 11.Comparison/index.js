35c100f2f529d657984d037ac884972c0a429a25
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-10 22:06:30 +0600

    Basic Comparison Learned

---
+++ 11.Comparison/index.js
@@ -0,0 +1,44 @@
+const mySandwich = 7;
+const brotherSandwich = 5;
+
+console.log(mySandwich > brotherSandwich, "I eat more sandwich");
+console.log(brotherSandwich > mySandwich, "Brother eat more sandwich");
+
+const studentsInMyClass = 45;
+const studentsInOtherClass = 50;
+
+console.log(
+  "More students in my class",
+  studentsInMyClass > studentsInOtherClass
+);
+console.log(
+  "More students in other class",
+  studentsInOtherClass > studentsInMyClass
+);
+
+const myMarks = 75;
+const friendMarks = 75;
+
+console.log("My marks is better than my friend", myMarks > friendMarks);
+console.log("My marks is not better than my friend", friendMarks > myMarks);
+console.log("My marks  equal my friends marks", friendMarks == myMarks);
+
+const savingsTarget = 100;
+const savings = 95;
+
+console.log("My target fill up is done", savingsTarget == savings);
+
+const number1 = 10;
+const number2 = 7;
+
+console.log("10 & 7 are not equal", number1 !== number2);
+
+const number3 = 20;
+const number4 = 15;
+
+console.log("20 is less than or equal 15", number3 <= number4);
+
+const myMarks2 = 45;
+const friendMarks2 = 97;
+
+console.log("My friend's marks is less than my marks", myMarks2 > friendMarks2);
