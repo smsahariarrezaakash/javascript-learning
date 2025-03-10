fb8de9eeee59642b428e35b6850f5485b1d65109
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-10 22:21:51 +0600

    Variable Comparison Learned

---
+++ 12.Variable-Comparison/index.js
@@ -0,0 +1,45 @@
+const myBrotherAge = 10;
+const myAge = 15;
+
+console.log(
+  "My brother age is less than or equal with me",
+  myBrotherAge <= myAge
+);
+
+const passTarget = 33;
+const myMarks = 45;
+
+console.log("Did I pass?", myMarks > passTarget);
+
+const jamburaPrice = 50;
+const KodbelPrice = 70;
+
+console.log("Jambura is cheaper than Kodbel", jamburaPrice < KodbelPrice);
+
+const booksIRead = 10;
+const booksFriendRead = 10;
+
+console.log(
+  "My friend's and my books number are same",
+  booksFriendRead === booksIRead
+);
+
+const myBiscuit = 15;
+const friendBiscuit = 20;
+
+console.log("My biscuit is less than my friend", myBiscuit < friendBiscuit);
+
+const realGoal = 2;
+const barsaGoal = 3;
+
+console.log("Real Madrid goals are less than Barcelona", realGoal < barsaGoal);
+
+const myName = "Rahim";
+const friendName = "Karim";
+
+console.log("My name and my friends name are equal", myName === friendName);
+
+const myStudy = true;
+const friendStudy = false;
+
+console.log("Did Me & My friend do same work", myStudy === friendStudy);
