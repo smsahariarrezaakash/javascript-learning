fd18886979248b058e71db74c59beac9a7b3488b
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-17 01:29:16 +0600

    if else learned

---
+++ 14.if-else/index.js
@@ -0,0 +1,41 @@
+const returnTime = 7;
+if (returnTime < 6) {
+  console.log("Amar ammu nasta khawabe");
+} else {
+  console.log("Amake jharu diye bari dibe");
+}
+
+const login = "success";
+if (login == "success") {
+  console.log("welcome");
+} else {
+  console.log("Get Lost");
+}
+
+const runningDistance = 5;
+if (runningDistance == 5) {
+  console.log("Chocolate khabo");
+} else {
+  console.log("Mota vuri hobe");
+}
+
+const marks = 85;
+if (marks > 80) {
+  console.log("Baba bike kine dibe");
+} else {
+  console.log("Baba bike kine dibe na");
+}
+
+const showTime = 10;
+if (showTime < 9) {
+  console.log("movie dekhbo");
+} else {
+  console.log("nake tel diye ghumabo");
+}
+
+const temperature = 26;
+if (temperature > 30) {
+  console.log("Air conditioner chalamu");
+} else {
+  console.log("kombol muri diye ghumamu");
+}
