99accea1f01e620c94eb0afbfc0a69e0c0694668
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-17 01:50:11 +0600

    && and || learned

---
+++ 15.and-or/index.js
@@ -0,0 +1,51 @@
+if (age > 18 && height > 60) {
+  console.log("gari thelbe");
+} else {
+  console.log("garite bose thakbe");
+}
+
+if (math > 80 || english > 85) {
+  console.log("vorti kora jabe");
+} else {
+  console.log("biye diye dibo");
+}
+
+if (gpa === 5 && income < 10000) {
+  console.log("scholarship pabe");
+} else {
+  console.log("beton diye porte hobe");
+}
+
+if (age2 < 30 && experience > 2) {
+  console.log("job exam dite parbe");
+} else {
+  console.log("job exam dite parbe na");
+}
+
+if (egg > 12 || hen === 0) {
+  console.log("Dimer korma ranna korbo");
+} else {
+  console.log("Pawruti r kola khabo");
+}
+
+if (temperature > 100 || cough === true) {
+  console.log("doctor r kache jao");
+} else {
+  console.log("katha muri diye suye thako");
+}
+
+if (attendance > 80 && homework === true) {
+  console.log("Exam dite parbe");
+} else {
+  console.log("Auto Fail");
+}
+
+if (current === true && mobileCharge === false) {
+  console.log("Porte bosbo");
+} else {
+  console.log("video game khelbo");
+}
+
+if (shirtPrice < 1000 && coupon === true) {
+  console.log("You will get 20% discount");
+}
