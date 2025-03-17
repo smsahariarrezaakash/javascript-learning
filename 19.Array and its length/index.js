a0c86257a44e4c776c138fd5f0b083750ac24e77
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-18 02:39:48 +0600

    Logical Not & Ternary operator learned

---
+++ 19.Array and its length/index.js
@@ -0,0 +1,43 @@
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+console.log(numbers);
+
+const fruitContainer = ["Apple", "Banana", "Orange", "Mango", "Guava"];
+console.log(fruitContainer);
+
+const vegetableContainer = [
+  "Potato",
+  "Onion",
+  "Eggplant",
+  "Radish",
+  "Carrot",
+  "Tomato",
+  "Ladies Finger",
+  "Gourd",
+  "Cucumber",
+  "Pointed Gourd",
+];
+console.log(vegetableContainer.length);
+
+const favoriteCinema = [
+  "Ironman",
+  "Batman",
+  "Superman",
+  "Avengers",
+  "Justice League",
+];
+console.log(favoriteCinema);
+
+let oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+console.log(oddNumbers);
+
+const colors = ["red", "black", "yellow"];
+console.log(colors.length);
+
+const capitalOfVariousCountries = [
+  "Dhaka",
+  "New-Delhi",
+  "Islamabad",
+  "Washington DC",
+];
+
+console.log(capitalOfVariousCountries.length);
