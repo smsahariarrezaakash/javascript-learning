6d984cc4e1022c6eaf0ee6ce199e1921c316ca46
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-19 01:39:59 +0600

    Array index learned

---
+++ 20.Array-index/index.js
@@ -0,0 +1,39 @@
+const friends = ["a", "b", "c", "d"];
+console.log(friends[3]);
+
+const favoriteBooks = [
+  "Quran",
+  "Ar Rahikul Makhtum",
+  "Dhulimolin Upohar Ramadan",
+  "Hisnul Muslim",
+  "Message",
+  "Paradoxical Sajid",
+  "Marhaba, Javascript e maro thaba",
+];
+console.log(favoriteBooks[5]);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[7] = 30;
+console.log(numbers);
+
+let games = ["free fire", "pubg", "candy crush", "temple run"];
+games[2] = "subway surfer";
+console.log(games);
+
+const familyMembers = ["a", "b", "c", "d", "e"];
+familyMembers[3] = "ddddd";
+console.log(familyMembers);
+
+const countriesIWantToVisit = [
+  "Saudi Arab",
+  "Indonesia",
+  "Malaysia",
+  "UAE",
+  "Turkey",
+  "Thailand",
+  "Japan",
+];
+console.log(countriesIWantToVisit[4]);
+
+const thingsOnMyDesk = ["mobile", "sunglass", "water bottle", "coffee cup"];
+console.log(thingsOnMyDesk[7]);
