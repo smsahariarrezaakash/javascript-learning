879d46f12c819243ded181f5fa853fbcb35c75ff
smsahariarrezaakash <smsahariarrezaakash2025@gmail.com>
2025-03-19 02:11:46 +0600

    push, pop, shift, unshift learned

---
+++ 21.Push-pop-shift-unshift/index.js
@@ -0,0 +1,25 @@
+const numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+console.log(numbers);
+
+const friends = ["sojib", "sagor", "sakib", "sohel"];
+friends.push("sumon");
+console.log(friends);
+
+const games = ["choices", "mechat", "love spark"];
+games.pop();
+console.log(games);
+
+const numbers2 = [24, 34, 48, 60];
+numbers2.unshift(12);
+console.log(numbers2);
+
+const books = [
+  "nofs r sathe loray",
+  "shoytan r sathe loray",
+  "shamayel e tirmiji",
+  "kotha bolo joytun brikkho",
+  "message",
+];
+books.shift();
+console.log(books);
